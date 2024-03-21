e0bd3d6556fa9f18cbcfa2b755ebd75ded836366
Paz Maria Grecht Lort <49251438+pazlort@users.noreply.github.com>
2024-03-21 13:13:17 -0300

    Delete fondo_del_barril.sql

--- fondo_del_barril.sql
+++
@@ -1,7 +0,0 @@
--- ¿Cuántas películas tiene IMBD de cada género ordenado por el más popular?
-SELECT COUNT(*) cant_de_pelis, movies_genres.genre genero
-FROM movies_genres
-INNER JOIN movies
-ON movies_genres.movie_id = movies.id
-GROUP BY genero
-ORDER BY cant_de_pelis DESC;
